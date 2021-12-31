3b304a2f98806a22b3ad57b641c3cef468643b01
Fedor Kuyanov <feodor.kuyanov@gmail.com>
2021-12-31 19:53:24 +0000

    Separate tests

--- test/test_masternode.cpp
+++ test/test_masternode.cpp
@@ -21,7 +21,7 @@ public:
 
     MasterNodeServer() : config("../masternode/config/test.json") {
         std::thread([*this] { Run(config); }).detach();
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 
     std::string PostQuery(const std::string &target, const std::string &body) {
@@ -31,8 +31,8 @@ public:
 
         auto results = resolver.resolve(config.host, std::to_string(config.port));
         stream.connect(results);
+
         http::request<http::string_body> req{http::verb::post, target, 10};
-        req.set(http::field::host, "localhost");
         req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
         req.set(http::field::content_length, boost::lexical_cast<std::string>(body.size()));
         req.body() = body;
@@ -51,17 +51,15 @@ bool IsUuid(const std::string &s) {
     return s.size() == 36 && s[8] == '-' && s[13] == '-' && s[18] == '-' && s[23] == '-';
 }
 
-bool IsParseError(const std::string &s) {
-    return s.starts_with("Could not parse json:");
+void CheckSubmitStartsWith(const std::string &body, const std::string &prefix) {
+    MasterNodeServer server;
+    auto result = server.PostQuery("/submit", body);
+    ASSERT_TRUE(result.starts_with(prefix));
 }
 
-bool IsValidationError(const std::string &s) {
-    return s.starts_with("Invalid document:");
-}
-
-bool IsSemanticError(const std::string &s) {
-    return s.starts_with("Semantic error:");
-}
+const char *parse_error_prefix = "Could not parse json:";
+const char *validation_error_prefix = "Invalid document:";
+const char *semantic_error_prefix = "Semantic error:";
 
 TEST(TestMasterNode, GraphIdUnique) {
     MasterNodeServer server;
@@ -76,46 +74,78 @@ TEST(TestMasterNode, GraphIdUnique) {
     ASSERT_EQ(results.size(), iter);
 }
 
-TEST(TestMasterNode, SubmitLarge) {
+TEST(TestMasterNode, SubmitMaxPayloadSize) {
     MasterNodeServer server;
     std::string body;
     body.resize(server.config.max_payload_size, '.');
     auto result = server.PostQuery("/submit", body);
-    ASSERT_TRUE(IsParseError(result));
+    ASSERT_TRUE(result.starts_with(parse_error_prefix));
     body.push_back('.');
     result = server.PostQuery("/submit", body);
     ASSERT_TRUE(result.empty());
 }
 
-TEST(TestMasterNode, GraphParseError) {
-    MasterNodeServer server;
-    std::vector<std::string> bodies = {
-        "", "{", "}", "{:}", "{,}", "{a:b}", "\"a\":\"b\"", "{[]:[]}", "{\"a\":\"b}", "{\"a\":2,}"};
-    for (const auto &body : bodies) {
-        auto result = server.PostQuery("/submit", body);
-        ASSERT_TRUE(IsParseError(result));
-    }
+TEST(TestMasterNode, GraphParseError1) {
+    CheckSubmitStartsWith("", parse_error_prefix);
+}
+TEST(TestMasterNode, GraphParseError2) {
+    CheckSubmitStartsWith("{", parse_error_prefix);
+}
+TEST(TestMasterNode, GraphParseError3) {
+    CheckSubmitStartsWith("}", parse_error_prefix);
+}
+TEST(TestMasterNode, GraphParseError4) {
+    CheckSubmitStartsWith("{:}", parse_error_prefix);
+}
+TEST(TestMasterNode, GraphParseError5) {
+    CheckSubmitStartsWith("{,}", parse_error_prefix);
+}
+TEST(TestMasterNode, GraphParseError6) {
+    CheckSubmitStartsWith("{a:b}", parse_error_prefix);
+}
+TEST(TestMasterNode, GraphParseError7) {
+    CheckSubmitStartsWith("\"a\":\"b\"", parse_error_prefix);
+}
+TEST(TestMasterNode, GraphParseError8) {
+    CheckSubmitStartsWith("{[]:[]}", parse_error_prefix);
+}
+TEST(TestMasterNode, GraphParseError9) {
+    CheckSubmitStartsWith("{\"a\":\"b}", parse_error_prefix);
+}
+TEST(TestMasterNode, GraphParseError10) {
+    CheckSubmitStartsWith("{\"a\":2,}", parse_error_prefix);
 }
 
-TEST(TestMasterNode, GraphValidationError) {
-    MasterNodeServer server;
-    std::vector<std::string> bodies = {
-        "{}",
-        "[]",
-        "{\"blocks\":[],\"connections\":[]}",
-        "{\"connections\":[],\"files\":[]}",
-        "{\"blocks\":[],\"files\":[]}",
-        "{\"blocks\":[],\"connections\":[],\"files\":0}",
-        "{\"blocks\":[],\"connections\":[],\"files\":{}}",
+TEST(TestMasterNode, GraphValidationError1) {
+    CheckSubmitStartsWith("{}", validation_error_prefix);
+}
+TEST(TestMasterNode, GraphValidationError2) {
+    CheckSubmitStartsWith("[]", validation_error_prefix);
+}
+TEST(TestMasterNode, GraphValidationError3) {
+    CheckSubmitStartsWith("{\"blocks\":[],\"connections\":[]}", validation_error_prefix);
+}
+TEST(TestMasterNode, GraphValidationError4) {
+    CheckSubmitStartsWith("{\"connections\":[],\"files\":[]}", validation_error_prefix);
+}
+TEST(TestMasterNode, GraphValidationError5) {
+    CheckSubmitStartsWith("{\"blocks\":[],\"files\":[]}", validation_error_prefix);
+}
+TEST(TestMasterNode, GraphValidationError6) {
+    CheckSubmitStartsWith("{\"blocks\":[],\"connections\":[],\"files\":0}",
+                          validation_error_prefix);
+}
+TEST(TestMasterNode, GraphValidationError7) {
+    CheckSubmitStartsWith("{\"blocks\":[],\"connections\":[],\"files\":{}}",
+                          validation_error_prefix);
+}
+TEST(TestMasterNode, GraphValidationError8) {
+    CheckSubmitStartsWith(
         "{\"blocks\":[{\"name\":\"block1\",\"inputs\":[{}],\"outputs\":[],\"tasks\":[]}],"
-        "\"connections\":[],\"files\":[]}"};
-    for (const auto &body : bodies) {
-        auto result = server.PostQuery("/submit", body);
-        ASSERT_TRUE(IsValidationError(result));
-    }
+        "\"connections\":[],\"files\":[]}",
+        validation_error_prefix);
 }
 
-TEST(TestMasterNode, GraphSemanticError) {
-    MasterNodeServer server;
+TEST(TestMasterNode, GraphSemanticError1) {
     // TODO
 }
