b36772c9af0fc4a95f3e88ab5c1916227c661bc5
Fedor Kuyanov <feodor.kuyanov@gmail.com>
2023-01-06 18:30:32 +0300

    Renamed targets

--- test/runner/test.sh
+++ test/runner/test.sh
@@ -3,8 +3,8 @@
 export DATA_DIR=$PWD/data
 export VAR_DIR=$PWD/var
 
-./$BUILD_DIR/runner/polyrunner --config runner/runner_conf.json &
-sleep 1 && ./$BUILD_DIR/test/runner/test_polyrunner
+./$BUILD_DIR/runner/prunner --config runner/runner_conf.json &
+sleep 1 && ./$BUILD_DIR/test/runner/test_prunner
 STATUS=$?
-pkill polyrunner
+pkill prunner
 exit $STATUS
--- test/scheduler/test.sh
+++ test/scheduler/test.sh
@@ -3,8 +3,8 @@
 export DATA_DIR=$PWD/data
 export VAR_DIR=$PWD/var
 
-./$BUILD_DIR/scheduler/polyscheduler --config scheduler/scheduler_conf.json &
-sleep 1 && ./$BUILD_DIR/test/scheduler/test_polyscheduler
+./$BUILD_DIR/scheduler/pscheduler --config scheduler/scheduler_conf.json &
+sleep 1 && ./$BUILD_DIR/test/scheduler/test_pscheduler
 STATUS=$?
-pkill polyscheduler
+pkill pscheduler
 exit $STATUS
