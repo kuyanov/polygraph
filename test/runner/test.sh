4cf093b46c00779d7b17d13f2f034bf0fcecb46b
Fedor Kuyanov <feodor.kuyanov@gmail.com>
2023-01-06 20:17:22 +0300

    Updated test scripts

--- test/runner/test.sh
+++ test/runner/test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-export DATA_DIR=$PWD/data
-export VAR_DIR=$PWD/var
+PROJ_DIR=$(realpath $BUILD_DIR/..)
+export DATA_DIR=$PROJ_DIR/data
+export VAR_DIR=$PROJ_DIR/var
 
-./$BUILD_DIR/runner/prunner --config runner/runner_conf.json &
+./$BUILD_DIR/runner/prunner --config $PROJ_DIR/runner/runner_conf.json 2>stderr.log &
 sleep 1 && ./$BUILD_DIR/test/runner/test_prunner
 STATUS=$?
 pkill prunner
+cat stderr.log && rm stderr.log
 exit $STATUS
--- test/scheduler/test.sh
+++ test/scheduler/test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-export DATA_DIR=$PWD/data
-export VAR_DIR=$PWD/var
+PROJ_DIR=$(realpath $BUILD_DIR/..)
+export DATA_DIR=$PROJ_DIR/data
+export VAR_DIR=$PROJ_DIR/var
 
-./$BUILD_DIR/scheduler/pscheduler --config scheduler/scheduler_conf.json &
+./$BUILD_DIR/scheduler/pscheduler --config $PROJ_DIR/scheduler/scheduler_conf.json 2>stderr.log &
 sleep 1 && ./$BUILD_DIR/test/scheduler/test_pscheduler
 STATUS=$?
 pkill pscheduler
+cat stderr.log && rm stderr.log
 exit $STATUS
