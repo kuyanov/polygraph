bc983412e2539af720e1e1f23fb663fdd0ed98a2
kuyanov <feodor.kuyanov@gmail.com>
2023-01-07 20:49:44 +0300

    Updated test scripts

--- test/runner/test.sh
+++ test/runner/test.sh
@@ -4,9 +4,9 @@ PROJ_DIR=$(realpath $BUILD_DIR/..)
 export DATA_DIR=$PROJ_DIR/data
 export VAR_DIR=$PROJ_DIR/var
 
-./$BUILD_DIR/runner/prunner --config $PROJ_DIR/runner/runner_conf.json 2>stderr.log &
-sleep 1 && ./$BUILD_DIR/test/runner/test_prunner
+mkdir -p $PROJ_DIR/logs
+$BUILD_DIR/runner/prunner --config $PROJ_DIR/runner/runner_conf.json 2>$PROJ_DIR/logs/runner.log &
+sleep 1 && $BUILD_DIR/test/runner/test_prunner
 STATUS=$?
 pkill prunner
-cat stderr.log && rm stderr.log
 exit $STATUS
--- test/scheduler/test.sh
+++ test/scheduler/test.sh
@@ -4,9 +4,9 @@ PROJ_DIR=$(realpath $BUILD_DIR/..)
 export DATA_DIR=$PROJ_DIR/data
 export VAR_DIR=$PROJ_DIR/var
 
-./$BUILD_DIR/scheduler/pscheduler --config $PROJ_DIR/scheduler/scheduler_conf.json 2>stderr.log &
-sleep 1 && ./$BUILD_DIR/test/scheduler/test_pscheduler
+mkdir -p $PROJ_DIR/logs
+$BUILD_DIR/scheduler/pscheduler --config $PROJ_DIR/scheduler/scheduler_conf.json 2>$PROJ_DIR/logs/scheduler.log &
+sleep 1 && $BUILD_DIR/test/scheduler/test_pscheduler
 STATUS=$?
 pkill pscheduler
-cat stderr.log && rm stderr.log
 exit $STATUS
