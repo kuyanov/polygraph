60609deef88262198ca18163c1913d7c25bc8f09
kuyanov <feodor.kuyanov@gmail.com>
2023-01-23 16:26:02 +0300

    Data & config one level up

--- test/scheduler/test.sh
+++ test/scheduler/test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 PROJ_DIR=$(realpath $BUILD_DIR/..)
-export CONF_DIR=$PROJ_DIR/config
-export DATA_DIR=$PROJ_DIR/data
+export CONF_DIR=$PROJ_DIR
+export DATA_DIR=$PROJ_DIR
 
 mkdir -p /var/log/polygraph
 $BUILD_DIR/scheduler/pscheduler 2>>/var/log/polygraph/common.log &
