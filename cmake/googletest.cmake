3af0811057c6d4a3cd84f544bbafd601377d272f
Fedor Kuyanov <feodor.kuyanov@gmail.com>
2023-07-20 22:02:27 +0200

    Separate cmake files for dependencies

---
+++ cmake/googletest.cmake
@@ -0,0 +1,9 @@
+include(FetchContent)
+set(FETCHCONTENT_QUIET FALSE)
+set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
+FetchContent_Declare(
+        googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG release-1.12.1
+)
+FetchContent_MakeAvailable(googletest)
